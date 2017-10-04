d08d8ab9827353d042e7354abe100711957aa4ac
Kaden Barlow <kadenbarlow@gmail.com>
2017-10-03 21:34:00 -0600

    Added basic api call for crime

--- api.js
+++ api.js
@@ -5,3 +5,20 @@ function initMap() {
         zoom: 8
     });
 }
+
+$(document).ready(function(){
+            $("#search").click(function(e){
+                $.ajax({
+                    url:'https://api.spotcrime.com/crimes.json?lat=40.2338438&lon=-111.65853370000002&radius=0.02&callback=jsonp1507087119154&key=privatekeyforspotcrimepublicusers-commercialuse-877.410.1607',
+                    dataType: "jsonp",
+                    complete: function (response) {
+                        //$('#output').html(response.responseText);
+                        console.log(response);
+                    },
+                    error: function () {
+                        $('#output').html('Bummer: there was an error!');
+                    },
+                });
+                return false;
+                });
+            });
