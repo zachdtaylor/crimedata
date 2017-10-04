b4cc0f3a7dc32eb25c5e681f45365ecc2c8e99f4
zacharytaylor <zachtylr04@gmail.com>
2017-10-03 21:35:25 -0600

    align

--- api.js
+++ api.js
@@ -7,18 +7,18 @@ function initMap() {
 }
 
 $(document).ready(function(){
-            $("#search").click(function(e){
-                $.ajax({
-                    url:'https://api.spotcrime.com/crimes.json?lat=40.2338438&lon=-111.65853370000002&radius=0.02&callback=jsonp1507087119154&key=privatekeyforspotcrimepublicusers-commercialuse-877.410.1607',
-                    dataType: "jsonp",
-                    complete: function (response) {
-                        //$('#output').html(response.responseText);
-                        console.log(response);
-                    },
-                    error: function () {
-                        $('#output').html('Bummer: there was an error!');
-                    },
-                });
-                return false;
-                });
-            });
+    $("#search").click(function(e){
+        $.ajax({
+            url:'https://api.spotcrime.com/crimes.json?lat=40.2338438&lon=-111.65853370000002&radius=0.02&callback=jsonp1507087119154&key=privatekeyforspotcrimepublicusers-commercialuse-877.410.1607',
+            dataType: "jsonp",
+            complete: function (response) {
+                //$('#output').html(response.responseText);
+                console.log(response);
+            },
+            error: function () {
+                $('#output').html('Bummer: there was an error!');
+            },
+        });
+        return false;
+    });
+});
